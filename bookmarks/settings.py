7e3dea396468388a42827f9fc17cd1cd34abf27f
stuartelimu <stuartelimu@gmail.com>
2021-06-07 18:42:37 +0300

    add auth settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
