71c74c5aeced43dbc99a132ce1bbbe84ee4b34ac
stuartelimu <stuartelimu@gmail.com>
2021-06-09 14:49:56 +0300

    add edit profile

--- account/urls.py
+++ account/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('edit/', views.edit, name='edit'),
     path('register/', views.register, name='register'),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
